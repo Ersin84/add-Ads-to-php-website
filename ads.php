aef6551141b4ec0cedb9ea35e5319aaba96e3bce
Iulian Ghepes Wromo <info.wromo@gmail.com>
2021-03-05 16:43:18 +0100

    Add file ads.php
    file ads.php example

---
+++ ads.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Displays the header content
+ *
+ * @package Theme Wromo
+ * @subpackage Wromo
+ * @since Wromo 1.0.1
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    
+  <ads>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168568850-1">
+  </script>
+  <script>
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+
+   gtag('config', 'UA-168568850-1');
+  </script>
+  
+  </ads>
